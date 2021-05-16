0911a2db459bfcc4a9fe8199d5fcd91723c11380
Juanda <danielalejandre1050@yahoo.com>
2021-05-16 00:24:20 -0500

    Merge Quinta con main

--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexion
 {
     private static final String CONTROLADOR = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/poo";
+    private static final String URL = "jdbc:mysql://localhost:3306/ametstravels";
     private static final String USUARIO = "root";
     private static final String CLAVE = "";
 
